85f129be8379a3c429be01266ba488ba8196afd8
cryst <crystianotv@gmail.com>
2026-01-28 20:25:37 +0100

    create repositories and provider for User

---
+++ src/Application/User/Provider/UserProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\User\Provider;
+
+use App\Domain\Entity\User;
+use App\Infrastructure\User\QueryRepository;
+
+final readonly class UserProvider
+{
+    public function __construct(private QueryRepository $repository)
+    {
+    }
+
+    public function loadById(string $id): ?User
+    {
+        return $this->repository->find($id);
+    }
+
+    /**
+     * @return User[]
+     */
+    public function loadAll(): array
+    {
+        return $this->repository->findAll();
+    }
+}
