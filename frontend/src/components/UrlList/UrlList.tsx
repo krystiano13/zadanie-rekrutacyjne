a26c9e65bc9072a4a050a26619c5e8e9bbcefe76
cryst <crystianotv@gmail.com>
2026-01-30 21:55:10 +0100

    Implement creating shortened link

--- frontend/src/components/UrlList/UrlList.tsx
+++ frontend/src/components/UrlList/UrlList.tsx
@@ -11,6 +11,35 @@ export function UrlList() {
     const [filter, setFilter] = useState('private');
     const [links, setLinks] = useState<Link[]>([]);
     const [isFetching, setIsFetching] = useState<boolean>(false);
+    const [fetchErrors, setFetchErrors] = useState<string[]>([]);
+
+    async function handleCreate(e: FormDataEvent) {
+        setIsFetching(true);
+        e.preventDefault();
+
+        const data = new FormData(e.target);
+
+        if (data.get("expireTime") === 'none') {
+            data.delete('expireTime');
+        }
+
+        await fetch('https://localhost/api/urls', {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`,
+            },
+            body: data
+        })
+            .then(res => res.json())
+            .then(result => {
+                if (result.error) {
+                    setFetchErrors([result.error]);
+                } else {
+                    setFilter('private');
+                }
+            })
+            .finally(() => setIsFetching(false))
+    }
 
     async function loadPublic() {
         setIsFetching(true);
@@ -29,20 +58,41 @@ export function UrlList() {
             .finally(() => setIsFetching(false))
     }
 
+    async function loadPrivate() {
+        setIsFetching(true);
+        await fetch('https://localhost/api/urls', {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                if(data.urls) {
+                    setLinks([]);
+                    setLinks(data.urls)
+                }
+            })
+            .finally(() => setIsFetching(false))
+    }
+
     useEffect(() => {
         if (filter === 'public') {
             loadPublic();
         }
+
+        if (filter === 'private') {
+            loadPrivate();
+        }
     }, [filter])
 
     useEffect(() => {
-        loadPublic();
+        loadPrivate();
     }, []);
 
     return (
         <div className="w-screen h-screen flex flex-col items-start px-8 sm:px-48 pb-12 lg:px-64">
             <h1 className="font-medium text-4xl pt-24">
-                Skracacz linków
+                URL Shortener
             </h1>
 
             <div className="pb-8">
@@ -50,38 +100,86 @@ export function UrlList() {
                     onClick={() => setFilter('public')}
                     className={`btn ${filter === 'public' ? 'btn-active' : ''}`}
                 >
-                    Publiczne linki
+                    Public links
                 </button>
                 <button
                     onClick={() => setFilter('private')}
                     className={`btn ${filter === 'private' ? 'btn-active' : ''}`}
                 >
-                    Twoje linki
+                    Your links
+                </button>
+                <button
+                    onClick={() => setFilter('create')}
+                    className={`btn ${filter === 'create' ? 'btn-active' : ''}`}
+                >
+                    Create link
                 </button>
             </div>
 
-            <div className="w-full h-auto flex-1 relative overflow-y-auto border-1">
-                <Spinner visible={isFetching} />
-                {
-                    links.map(item => (
-                        <div className="flex justify-between p-2 border-b-1">
-                            <div className="flex items-center gap-1">
-                                <label>{ item.url }</label>
-                                <label>kod: { item.code }</label>
-                                <label>{ item.alias ? item.alias : '(brak aliasu)' }</label>
+            {
+                filter !== 'create' &&
+                <div className="w-full h-auto flex-1 relative overflow-y-auto border-1">
+                    <Spinner visible={isFetching} />
+                    {
+                        links.map(item => (
+                            <div className="flex justify-between p-2 border-b-1">
+                                <div className="flex items-center gap-1">
+                                    <label>{ item.url }</label>
+                                    <label>kod: { item.code }</label>
+                                    <label>{ item.alias ? item.alias : '(brak aliasu)' }</label>
+                                </div>
+                                <div className="flex items-center">
+                                    <button className="btn btn-active mt-0!">Copy</button>
+                                    <button className="btn btn-active mt-0!">Stats</button>
+                                    {
+                                        filter === 'private' &&
+                                        <button className="btn btn-active mt-0!">Delete</button>
+                                    }
+                                </div>
                             </div>
-                            <div className="flex items-center">
-                                <button className="btn btn-active mt-0!">Kopiuj</button>
-                                <button className="btn btn-active mt-0!">Statystyki</button>
-                                {
-                                    filter === 'private' &&
-                                    <button className="btn btn-active mt-0!">Usuń</button>
-                                }
-                            </div>
-                        </div>
-                    ))
-                }
-            </div>
+                        ))
+                    }
+                </div>
+            }
+
+            {
+                filter === 'create' &&
+                <form onSubmit={handleCreate} className="flex w-100 flex-col gap-2 items-start">
+                    {
+                        fetchErrors.map((error: string) => (
+                            <p className="p-2 my-2 border border-red-500 text-red-500 bg-red-200">
+                                { error }
+                            </p>
+                        ))
+                    }
+
+                    <input name="url" className="w-full" placeholder="URL" required />
+
+                    <div className="w-full">
+                        <label>Visibility:</label>
+                        <br />
+                        <select name="type" className="w-full">
+                            <option value="private">Private</option>
+                            <option value="public">Public</option>
+                        </select>
+                    </div>
+
+                    <div className="w-full">
+                        <label>Expiration Time:</label>
+                        <br />
+                        <select name="expireTime" className="w-full">
+                            <option value="none">None</option>
+                            <option value="1h">1 hour</option>
+                            <option value="1d">1 day</option>
+                            <option value="1t">1 week</option>
+                        </select>
+                    </div>
+
+                    <input name="alias" className="w-full" placeholder="Alias" />
+
+                    <button disabled={isFetching} className="btn btn-active">Create</button>
+                </form>
+            }
         </div>
     )
 }
